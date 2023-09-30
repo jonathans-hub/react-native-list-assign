9fa49678919c7654a48f9d0561a9a905b34bf5b3
Jonathan <jadjekwei@gmail.com>
2023-09-30 01:15:08 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,54 +1,128 @@
 import React from "react";
-import { View, Text,  StyleSheet , Image} from "react-native";
-import profile from './assets/wp10721692-hapebeast-nft-wallpapers.jpg'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={style.container}>
-      <Image source={profile} style={style.image}/>    
-      <View style={style.deetz}>
-         <Text style={style.lable}>Name</Text>
-         <Text style={[style.lable, style.info]}>Bored Ape</Text>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+  render() {
+    return (
+      <View style={style.container}>
+        <View style={style.containerDiv}>
+          <View style={style.signInBox}>
+            <Text style={style.loginText}>Log in</Text>
+            <TextInput
+              style={[style.textInputArea, { marginBottom: 50 }]}
+              // secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="username"
+              value={this.state.username}
+              onChangeText={(username) => {
+                this.setState({ username });
+                console.log(this.state.username);
+              }}
+            />
+            <TextInput
+              style={style.textInputArea}
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="password"
+              value={this.state.password}
+              onChangeText={(password) => {
+                this.setState({ password });
+                console.log(this.state.password);
+              }}
+            />
+            <View style={style.forgetTO}>
+              <TouchableOpacity>
+                <Text style={style.forgetTOText}>Forgot password?</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={style.logButtonTO}>
+              <Text style={style.LogButtonText}>Log in</Text>
+            </TouchableOpacity>
+            <View style={style.EndText}>
+              <Text style={{fontSize: 17}}>Don't have an account?</Text>
+              <TouchableOpacity>
+              <Text style={style.signUpText}> Sign up</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
-      <View style={style.deetz}>
-         <Text style={style.lable}>Email</Text>
-         <Text style={[style.lable, style.info]}>boredape@nft.com</Text>
-      </View>
-      <View style={style.deetz}>
-         <Text style={style.lable}>Gender</Text>
-         <Text style={[style.lable, style.info]}>Male</Text>
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 120
-
   },
-  image:{
-    width: 140,
-    height:140,
-    alignSelf:"center",
-    marginBottom:50,
-    borderRadius:70
+  containerDiv: {
+    marginTop: 100,
+    margin: 40,
+    borderColor: "lightgrey",
+    borderWidth: 4,
+    borderRadius: 20,
+    height: 730,
   },
-  deetz:{
-    flexDirection: "row",
-    justifyContent:"space-around",
-    margin:10
+  signInBox: {
+    padding: 20,
   },
-  lable:{
-    fontSize:20,
-    flex:3,
-    borderColor:"black",
-    borderWidth:1, 
-    marginRight:10,
-    paddingHorizontal:10
+  loginText: {
+    marginVertical: 70,
+    fontSize: 40,
+    color: "indigo",
+    fontWeight: "bold",
   },
-  info:{
-    flex:6
-  }
+  textInputArea: {
+    borderBottomColor: "indigo",
+    borderBottomWidth: 2,
+    paddingBottom: 10,
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  forgetTO: {
+    alignItems: "flex-end",
+  },
+  forgetTOText: {
+    fontSize: 18,
+    color: "#24a0ed",
+  },
+  logButtonTO: {
+    marginTop: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    backgroundColor: "indigo",
+    borderRadius: 20,
+    marginBottom: 50,
+  },
+  LogButtonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  EndText:{
+    flexDirection:"row",
+    alignSelf:"center"
+  },
+  signUpText: {
+    fontSize: 17,
+    color: "indigo",
+},
 });
